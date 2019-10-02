f55da363dd8082b836333bfc41c5bab604541895
Lúcás Meier <cronokirby@gmail.com>
2019-10-02 13:46:01 +0200

    Add panic function

--- cici.c
+++ cici.c
@@ -1,4 +1,11 @@
+#include "stdlib.h"
 #include "stdio.h"
+
+// Exit the program with a given error message
+void panic(char const* msg) {
+    puts(msg);
+    exit(-1);
+}
 
 int main() {
     puts("a");
