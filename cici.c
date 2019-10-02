39fae26cb48430f5cf1733ce22e781344f06de0d
Lúcás Meier <cronokirby@gmail.com>
2019-10-02 13:59:37 +0200

    Add definition of Token

--- cici.c
+++ cici.c
@@ -1,11 +1,46 @@
-#include "stdlib.h"
+#include "stdbool.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 // Exit the program with a given error message
-void panic(char const* msg) {
+void panic(char const *msg) {
     puts(msg);
     exit(-1);
 }
+
+/** LEXING **/
+
+// Represents a type of token our lexer produces.
+typedef enum TokenType {
+    // Single character tokens
+    T_LEFT_PARENS,
+    T_RIGHT_PARENS,
+    T_LEFT_BRACE,
+    T_RIGHT_BRACE,
+    T_SEMICOLON,
+    // Keywords
+    T_INT,
+    T_MAIN,
+    T_RETURN,
+    // Litteral
+    T_LITT_NUMBER
+} TokenType;
+
+// Represents a type of litteral value we've lexed out
+typedef union Litteral {
+    // This variant holds a numeric litteral
+    int number;
+    // This variant can be set to tell us that no litteral is there
+    bool empty;
+} Litteral;
+
+// Represents a token produced in the lexing phase.
+typedef struct Token {
+    // Holds which type of token this is.
+    TokenType type;
+    // Holds information about the litteral contained in this token.
+    Litteral litt;
+} Token;
 
 int main() {
     puts("a");
